bd216d7de818bfe54c97c424972491e948367070
tode <wnstlrrla1@naver.com>
2021-08-17 18:50:33 +0900

    mustache

--- src/main/java/com/narangdesign/springboot/service/PostsService.java
+++ src/main/java/com/narangdesign/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.narangdesign.springboot.service;
 
 import com.narangdesign.springboot.domain.posts.Posts;
 import com.narangdesign.springboot.domain.posts.PostsRepository;
+import com.narangdesign.springboot.web.dto.PostsListResponseDto;
 import com.narangdesign.springboot.web.dto.PostsResponseDto;
 import com.narangdesign.springboot.web.dto.PostsSaveRequestDto;
 import com.narangdesign.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
