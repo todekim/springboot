68f44e0020fa35d3e6d2fe6c484e41d4d38cc52b
tode <wnstlrrla1@naver.com>
2021-08-18 18:08:53 +0900

    oauth

--- src/main/java/com/narangdesign/springboot/web/IndexController.java
+++ src/main/java/com/narangdesign/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.narangdesign.springboot.web;
 
+import com.narangdesign.springboot.config.auth.LoginUser;
+import com.narangdesign.springboot.config.auth.dto.SessionUser;
 import com.narangdesign.springboot.service.PostsService;
 import com.narangdesign.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -11,16 +13,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
